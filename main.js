27b1623d1a0fbf148e1b87fee27e73d335e91406
IVAR AHTSERHS <ravistha869@gmail.com>
2023-01-11 23:22:09 +0545

    second commit

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 const canvas = document.querySelector("#draw");
 const ctx = canvas.getContext("2d");
-console.log(ctx);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 ctx.strokeStyle = "blue";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
-ctx.lineWidth = 20;
+ctx.lineWidth = 1;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
   if (!isDrawing) return; // return is mouse is not down
 
-  console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   //start from
   ctx.moveTo(lastX, lastY);
   // goto
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY];
+  [lastX, lastY] = [e.offsetX, e.offsetY]; // destructuring
+  hue++;
+
+  if (hue >= 360) hue = 0;
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) direction = !direction;
+  if (direction) ctx.lineWidth++;
+  else ctx.lineWidth--;
 }
 
 canvas.addEventListener("mousemove", draw);
